6c11a9166ef05995e5c69a836480aa246db8ea54
Rafaelly Rodrigues <rrafasrodrigues@gmail.com>
2022-08-26 20:24:07 -0300

    initial functionalityadd update remote

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React from 'react'
+import { useTracker } from 'meteor/react-meteor-data'
+import { TasksCollection } from '/imports/api/TasksCollection'
+import { Task } from './Task'
+import { TaskForm } from './TaskForm'
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+}
+const deleteTask = ({ _id }) => TasksCollection.remove(_id)
+
+export const App = () => {
+  const tasks = useTracker(() =>
+    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+  )
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-bar">
+            <h1>Satellist</h1>
+          </div>
+        </div>
+      </header>
+      <div className="main">
+        <TaskForm />
+
+        <ul className="tasks">
+          {tasks.map(task => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
